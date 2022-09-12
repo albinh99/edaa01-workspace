fe12336e24827b550feb621a8d462e4daa5201a6
albinhansson <albiin1@live.se>
2022-09-12 10:27:35 +0200

    :)

--- lab1/src/Game/Board.java
+++ lab1/src/Game/Board.java
@@ -1,4 +1,21 @@
 package Game;
 
 public class Board {
+    private int noPins;
+    public Board(int noPins) {
+        this.noPins = noPins;
+    }
+
+    public void setUp(int pinnarStart){
+    }
+
+    public void takePins(int taPinnar){
+
+    }
+
+    public int getNoPins(){
+
+    }
+
+
 }
