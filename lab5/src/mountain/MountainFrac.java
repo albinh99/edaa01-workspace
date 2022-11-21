db8d7b5bf9099766e2d1b4bb917d6ea484809669
albinhansson <albiin1@live.se>
2022-11-21 19:59:37 +0100

    :)

--- lab5/src/mountain/MountainFrac.java
+++ lab5/src/mountain/MountainFrac.java
@@ -41,7 +41,6 @@ public class MountainFrac extends Fractal {
 
     private void fractalLine(TurtleGraphics g, int order, Point p1, Point p2, Point p3) {
         sides = new HashMap<>();
-        Side side;
 
         if (order == 0) {
             g.moveTo(p1.getX(),p1.getY());
@@ -51,31 +50,38 @@ public class MountainFrac extends Fractal {
 
         } else {
             int dev1 = (int) Math.round(RandomUtilities.randFunc(dev));
+            Side side13 = new Side(p1,p3);
+            Side side32 = new Side(p3,p2);
+            Side side21 = new Side(p2,p1);
 
-            if(!sides.containsKey(new Side(p1,p3))){
+            if(!sides.containsKey(side13)){
                 Point mid13 = new Point(dev1+p1.getX()+(p3.getX()-p1.getX())/2, dev1+p1.getY()+(p3.getY()-p1.getY())/2);
-                sides.put(new Side(p1,p3), mid13);
+                sides.put(side13, mid13);
             }
-            if(!sides.containsKey(new Side(p3,p2))){
+            if(!sides.containsKey(side32)){
                 Point mid32 = new Point(dev1+p2.getX()+(p3.getX()-p2.getX())/2, dev1+p2.getY()+(p3.getY()-p2.getY())/2);
-                sides.put(new Side(p3,p2), mid32);
+                sides.put(side32, mid32);
             }
-            if(!sides.containsKey(new Side(p2,p1))){
+            if(!sides.containsKey(side21)){
                 Point mid21 = new Point(dev1+p2.getX()+(p1.getX()-p2.getX())/2, dev1+p1.getY()+(p2.getY()-p1.getY())/2);
-                sides.put(new Side(p2,p1), mid21);
-
+                sides.put(side21, mid21);
             }
-            g.moveTo(mid13.getX(),mid13.getY());
-            g.forwardTo(mid32.getX(),mid32.getY());
-            g.forwardTo(mid21.getX(),mid21.getY());
-            g.forwardTo(mid13.getX(),mid13.getY());
+            int x13 = sides.get(side13).getX();
+            int y13 = sides.get(side13).getY();
+            int x32 = sides.get(side32).getX();
+            int y32 = sides.get(side32).getY();
+            int x21 = sides.get(side21).getX();
+            int y21 = sides.get(side21).getY();
 
-            fractalLine(g,order-1,mid13,mid32,mid21);
-            fractalLine(g,order-1,mid13,p3,mid32);
-            fractalLine(g,order-1,p1,mid13,mid21);
-            fractalLine(g,order-1,mid21,mid32,p2);
+            g.moveTo(x13,y13);
+            g.forwardTo(x32,y32);
+            g.forwardTo(x21,y21);
+            g.forwardTo(x13,y13);
 
-
+            fractalLine(g,order-1,sides.get(side13),sides.get(side32),sides.get(side21));
+            fractalLine(g,order-1,sides.get(side13),p3,sides.get(side32));
+            fractalLine(g,order-1,p1,sides.get(side13),sides.get(side21));
+            fractalLine(g,order-1,sides.get(side21),sides.get(side32),p2);
 
         }
     }
